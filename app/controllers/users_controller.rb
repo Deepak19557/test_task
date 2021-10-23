81d61d7c06bc0057cdfb2364880d87e981f510c3
Deepak Chourasiya <chourasiya123deepak@gmail.com>
2021-10-24 01:31:34 +0530

    credit_limit & credibility

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,18 +8,33 @@ class UsersController < ApplicationController
   end
 
   def credit_limit_formula
-    credit_limit = maximum_possible_emi * term_in_months
-    maximum_possible_emi = (monthly_recurring_inflow/2 - monthly_recurring_outlfow)
+    maximum_possible_emi = ((User.last.money_inflow_in_account).to_i/2 - (User.last.money_outflow_in_account).to_i)
+    if maximum_possible_emi > 0 and maximum_possible_emi < 5000
+    term_in_months = 6 
+    elsif maximum_possible_emi > 5001 and maximum_possible_emi < 10000
+      term_in_months = 12
+    elsif maximum_possible_emi > 10001 and maximum_possible_emi < 20000
+      term_in_months = 18
+    elsif maximum_possible_emi > 20000
+      term_in_months = 24
+    end
+
+    @credit_limit = maximum_possible_emi * term_in_months
+
+    if User.last.score.to_i > 2
+      @message = "Your loan is accepted"
+    else
+      @message = "your loan is rejected"
+    end
   end
 
-  def credibility_score
-    debugger
-  end
 
   def create
     @user = User.new(user_params)
+    score = params[:user][:facebook].to_i + params[:user][:linkedin].to_i + params[:user][:twitter].to_i + params[:user][:already_loan].to_i
+    @user.score = score
     if @user.save
-      redirect_to @user
+      redirect_to credit_limit_formula_path
     else
       render :new
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_22_074954) do
+ActiveRecord::Schema.define(version: 2021_10_23_160302) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_10_22_074954) do
     t.string "money_outflow_in_account"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "score"
   end
 
 end
